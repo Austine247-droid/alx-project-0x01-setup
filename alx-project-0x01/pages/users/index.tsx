4ce6b2c00e894af7a986d8571015bf1112b9ed24
Austine247-droid <ugberaeseoa@gmail.com>
2025-05-31 05:58:58 +0000

    Set up navigation between pages

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import Header from "@/components/layout/Header";
 
 const index = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 };
 
 export default index;
